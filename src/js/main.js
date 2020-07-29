066f2f164aa98ef75a7102f219eeba17d721372c
shimanamisan <49751604+shimanamisan@users.noreply.github.com>
2020-07-29 17:11:39 +0900

    commit group!

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
-const $ = require('jquery');
+var $ = require("jquery");
 
-$(function(){
+$(function () {
+  var $jsScrollHeader = $(".js-scroll-trigger");
+  var $jsHeaderLogo = $(".js-p-header__logo");
 
- 
+  $(window).scroll(() => {
+    if ($(window).scrollTop() >= 300) {
+      //   console.log($(window).scrollTop());
+      $jsScrollHeader.addClass("c-anime__scroll");
+      $jsHeaderLogo.addClass("p-header__scroll");
+    } else {
+      $jsScrollHeader.removeClass("c-anime__scroll");
+      $jsHeaderLogo.removeClass("p-header__scroll");
+    }
+  });
+
+  
+
+  console.log($(window).innerWidth())
 });
