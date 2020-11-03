a240e8f830dcbf13852b967eea8711c777ca4a79
shimanamisan <mikan.sup.all@gmail.com>
2020-11-03 20:31:30 +0900

    スタイリングなどを修正

--- dist/footer.php
+++ dist/footer.php
@@ -1,4 +1,7 @@
     <footer id="footer" class="l-footer p-footer">
+      <div class="p-footer__pagetop js-pagetop">
+        <a href="#"><img src="./img/pagetop.png" alt=""></a>
+      </div>
     <p class="p-footer__text">
       Copyright© LINEUP BASEBALLCULB All Rights Reserved.
     </p>
