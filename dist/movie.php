ee798de8f037f94e1e600bfdace70b9ea4b71715
shimanamisan <mikan.sup.all@gmail.com>
2021-01-31 00:00:00 +0900

    [add] 新しく動画スライド用要素をPHPファイルに切り出し

---
+++ dist/movie.php
@@ -0,0 +1,49 @@
+              <li class="p-homeCategory__movie__item p-slider__item">
+              <iframe class="p-youtube" src="https://www.youtube.com/embed/w4C5sTF6_Wc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </li>
+              <li class="p-homeCategory__movie__item p-slider__item">
+              <iframe
+                  class="p-youtube"
+                  src="https://www.youtube.com/embed/IH52aBOIhPU" 
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+                </iframe>
+              </li>
+              <!-- start ここが最初中央に配置される -->
+              <li class="p-homeCategory__movie__item p-slider__item">
+              <iframe class="p-youtube" 
+                src="https://www.youtube.com/embed/jhkh5lZDLy8"
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+                </iframe>
+              </li>
+              <!-- end ここが最初中央に配置される -->
+              <li class="p-homeCategory__movie__item p-slider__item">
+              <iframe 
+                class="p-youtube" 
+                src="https://www.youtube.com/embed/UkvG0f7kw9s" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+                </iframe>
+              </li>
+              <li class="p-homeCategory__movie__item p-slider__item">
+                <iframe 
+                  class="p-youtube"
+                  src="https://www.youtube.com/embed/KENrLCcGzOw"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen>
+                </iframe>
+              </li>
+              <li class="p-homeCategory__movie__item p-slider__item">
+                <iframe
+                  class="p-youtube"
+                  src="https://www.youtube.com/embed/n6uQNIvgLco"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen
+                ></iframe>
+              </li>
